e0c957729fe6b62d3819981d954942b4c599e94f
rajesh11cse <rajeshkumar11cse@gmail.com>
2026-02-28 14:45:34 +0530

    init app

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+// AI-generated Jest config for e2e tests
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/tests/e2e/**/*.test.ts'],
+  testTimeout: 30000,
+};
+
